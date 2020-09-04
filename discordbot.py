115f007b714ed5f19918b8ad02eb5b39b10bdeac
doi7477 <70732724+doi7477@users.noreply.github.com>
2020-09-04 11:55:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,30 +4,30 @@ from discord.ext import tasks
 import os
 import traceback
 
-#bot = commands.Bot(command_prefix='/')
-CHANNEL_ID = 751149121876000851
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+#CHANNEL_ID = 751149121876000851
+#client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-#@bot.event
-#async def on_command_error(ctx, error):
-#    orig_error = getattr(error, "original", error)
-#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-#    await ctx.send(error_msg)
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 
-#@bot.command()
-#async def ping(ctx):
-#    await ctx.send('pong')
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-@tasks.loop(seconds=3)
-async def loop():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('時間だよ')  
+#@tasks.loop(seconds=3)
+#async def loop():
+#    channel = client.get_channel(CHANNEL_ID)
+#    await channel.send('時間だよ')  
 
 #ループ処理実行
-loop.start()
-client.run(TOKEN)
+#loop.start()
+#client.run(TOKEN)
 
-#bot.run(token)
+bot.run(token)
