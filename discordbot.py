38fb285b8e409a1608b490d85cad5652b0c1cf08
doi7477 <70732724+doi7477@users.noreply.github.com>
2020-09-04 16:02:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,8 @@ import os
 import traceback
 
 token = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 751149121876000851
+#せとうぽ-要塞攻略室
+CHANNEL_ID = 713535093469347955
 client = discord.Client()
 #bot = commands.Bot(command_prefix='/')
 
@@ -15,7 +16,6 @@ client = discord.Client()
 #    orig_error = getattr(error, "original", error)
 #    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
 #    await ctx.send(error_msg)
-
 
 #@bot.command()
 #async def ping(ctx):
@@ -26,23 +26,15 @@ async def loop():
     #現在時刻取得
     JST = timezone(timedelta(hours=+9), 'JST')
     now = datetime.now(JST).strftime('%H:%M')
-    if now == '15:58': 
-        channel = client.get_channel(751149121876000851)
+    
+    if now == '21:00': 
+        channel = client.get_channel(CHANNEL_ID)
         if channel is None:
             pass
         else:
-            await channel.send('@everyone 正解' + now)
-    else:
-        channel = client.get_channel(751149121876000851)
-        if channel is None:
-            pass
-        else:
-            await channel.send('@everyone ちゃいます' + now)
-#    client.send_message(client.get_channel('751149121876000851'), 'hello')
+            await channel.send('@everyone 要塞だよ！全員集合！！' + now)
 
 #ループ処理実行
 loop.start()
 client.run(token)
-
-
 #bot.run(token)
