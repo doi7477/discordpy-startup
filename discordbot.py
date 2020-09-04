ff998ec0de7d11d8335d1f71e43d35f0c72edc03
doi7477 <70732724+doi7477@users.noreply.github.com>
2020-09-04 11:44:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,25 @@
 import discord
-from discord.ext import commands
+#from discord.ext import commands
 from discord.ext import tasks
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+#bot = commands.Bot(command_prefix='/')
 CHANNEL_ID = 751149121876000851
+client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+#@bot.event
+#async def on_command_error(ctx, error):
+#    orig_error = getattr(error, "original", error)
+#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#    await ctx.send(error_msg)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+#@bot.command()
+#async def ping(ctx):
+#    await ctx.send('pong')
 
 @tasks.loop(seconds=3)
 async def loop():
@@ -27,5 +28,6 @@ async def loop():
 
 #ループ処理実行
 loop.start()
-    
-bot.run(token)
+client.run(TOKEN)
+
+#bot.run(token)
