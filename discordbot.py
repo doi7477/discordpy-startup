edf71d654450e025f1ecb6b1e2f3507215abda24
doi7477 <70732724+doi7477@users.noreply.github.com>
2021-03-22 22:35:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -43,6 +43,17 @@ async def loop():
             #await channel.send('OK. In your words I will start booting.')
             #pass
 
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+
 #ループ処理実行
 loop.start()
 client.run(token)
