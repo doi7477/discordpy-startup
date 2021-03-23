8d857099f9fb3f6251a95a1ca7b8dd3b77dc770c
doi7477 <70732724+doi7477@users.noreply.github.com>
2021-03-23 11:18:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,10 +5,14 @@ from datetime import datetime, timedelta, timezone
 import os
 import traceback
 
-token = os.environ['DISCORD_BOT_TOKEN']
 #せとうぽ-要塞攻略室
 CHANNEL_ID = 713535093469347955
+fort_notice_flg = 1
+
+token = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
+
+
 #bot = commands.Bot(command_prefix='/')
 
 #@bot.event
@@ -23,32 +27,48 @@ client = discord.Client()
 
 @tasks.loop(seconds=60)
 async def loop():
+    
+    # 通知オフなら処理しない
+    if fort_notice_flg == 0:
+        return
+        
     #現在時刻取得
     JST = timezone(timedelta(hours=+9), 'JST')
     now = datetime.now(JST).strftime('%H:%M')
-    
+        
+    # 21:00に要塞通知を行う
     if now == '21:00': 
         channel = client.get_channel(CHANNEL_ID)
         if channel is None:
             pass
-        else:
+        else:          
             await channel.send('@everyone 要塞だよ！全員集合！！')
             #pass
-            
-    #if now == '13:30': 
-        #channel = client.get_channel(CHANNEL_ID)
-        #if channel is None:
-            #pass
-        #else:
-            #await channel.send('OK. In your words I will start booting.')
-            #pass
-
+                
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
+    
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
+    
+    # 要塞通知ON設定
+    if message.content == '/せとうぽ 通知オン':
+        if fort_notice_flg == 1:
+            await message.channel.send('すでに要塞通知設定はONです')
+        else:
+            fort_notice_flg = 1:
+            await message.channel.send('要塞通知をONに設定しました')
+
+    # 要塞通知OFF設定
+    if message.content == '/せとうぽ 通知オフ':
+        if fort_notice_flg == 0:
+            await message.channel.send('すでに要塞通知設定はOFFです')
+        else:
+            fort_notice_flg = 0:
+            await message.channel.send('要塞通知をOFFに設定しました')
+    
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
