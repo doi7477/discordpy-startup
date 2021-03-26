ef6921698afcf15efb8acb4921da4f4acbb0835a
doi7477 <70732724+doi7477@users.noreply.github.com>
2021-03-26 17:49:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,44 +1,49 @@
-import discord
-from discord.ext import tasks
-#from discord.ext import commands
-from datetime import datetime, timedelta, timezone
 import os
 import random
 import traceback
-#import configparser
+from datetime import datetime, timedelta, timezone
 
+import discord
+from discord.ext import tasks
+
+#Discord設定
 token = os.environ['DISCORD_BOT_TOKEN']
-
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-#client = discord.Client()
-
 presence = discord.Game('モンスターハンターライズ')
 
+#せとうぽ サーバID
+SERVER_ID = 578209286639976448
 #せとうぽ-雑談
 ZATUDAN_CHANNEL_ID = 578209286639976452
 #せとうぽ-要塞攻略室
 YOUSAI_CHANNEL_ID = 713535093469347955
 #せとうぽ-幹部用
 KANBU_CHANNEL_ID = 605428683364106288
+
 #どい動物園-雑談
 DEBUG_CHANNEL_ID = 751149121876000851
+#どい動物園 サーバーID
+DEBUG_SERVER_ID = 751149121284603935
+
 #要塞通知フラグ
 g_yousai_notice_flg = 1
 
 #全体ヘルプ文
-embed = discord.Embed(title="**コマンドリスト一覧**",description="",color=0x4169E1)
-embed.add_field(name="**/せとうぽ**",value="--> ヘルプを呼び出します",inline=False)
-embed.add_field(name="**/せとうぽ おみくじ**",value="--> おみくじを引きます",inline=False)
-embed.add_field(name="**/せとうぽ ぜくの装備消去**",value="--> ぜくしーをせくしーにします",inline=False)
+embed_help = discord.Embed(title="**コマンドリスト一覧**",description="",color=0x4169E1)
+embed_help.add_field(name="**/せとうぽ**",value="--> ヘルプを呼び出します",inline=False)
+embed_help.add_field(name="**/せとうぽ おみくじ**",value="--> おみくじを引きます",inline=False)
+embed_help.add_field(name="**/せとうぽ ぜくの装備消去**",value="--> ぜくしーをせくしーにします",inline=False)
 
 #幹部用ヘルプ文
-embed2 = discord.Embed(title="**管部用コマンドリスト一覧**",description="",color=0x4169E1)
-embed2.add_field(name="**/せとうぽ 要塞通知オン**",value="--> 21時の要塞通知をオンにします\r\n",inline=False)
-embed2.add_field(name="**/せとうぽ 要塞通知オフ**",value="--> 21時の要塞通知をオフにします\r\n",inline=False)
+embed_kanbu_help = discord.Embed(title="**管部用コマンドリスト一覧**",description="",color=0x4169E1)
+embed_kanbu_help.add_field(name="**/せとうぽ 要塞通知オン**",value="--> 21時の要塞通知をオンにします\r\n",inline=False)
+embed_kanbu_help.add_field(name="**/せとうぽ 要塞通知オフ**",value="--> 21時の要塞通知をオフにします\r\n",inline=False)
 
 #テストようこそ文
-strtest = "どい丸"
+strtit = "ようこそ せとうぽへ"
+intcol = 0x4169E1
+strpng = "https://img.altema.jp/altema/uploads/2019/03/2019y03m07d_1405336875.png"
 strtmp = "以下で自己紹介をお願いします\r\n"\
          " <#771510773549629480> \r\n"\
          "\r\n"\
@@ -54,9 +59,10 @@ strtmp = "以下で自己紹介をお願いします\r\n"\
          "以下コマンドでご確認ください\r\n/せとうぽ\r\n"\
          "\r\n"\
          "不明点は気軽に連絡ください\r\n"
-embed_t = discord.Embed(title="ようこそ せとうぽへ",description="",color=0x4169E1)
-embed_t.add_field(name=f":sparkles:{strtest}さん:sparkles:\r\nご参加ありがとうございます",value=strtmp,inline=False)
-embed_t.set_thumbnail(url="https://img.altema.jp/altema/uploads/2019/03/2019y03m07d_1405336875.png")
+
+embed_test = discord.Embed(title=strtit,description="",color=intcol)
+embed_test.add_field(name=":sparkles:テストさん:sparkles:\r\nご参加ありがとうございます",value=strtmp,inline=False)
+embed_test.set_thumbnail(url=strpng)
 
 ##################### 要塞通知処理 #####################
 @tasks.loop(seconds=60)
@@ -87,7 +93,11 @@ async def loop():
 async def on_ready():                
     # メッセージ受信時に動作する処理
     print('せとうぽくん起動しました')
+
+    #プレイ中更新
     await client.change_presence(activity=presence)
+    
+    #幹部用 起動通知
     channel = client.get_channel(KANBU_CHANNEL_ID)
     if channel is None:
         pass
@@ -95,59 +105,47 @@ async def on_ready():
         #await channel.send('せとうぽくん起動しました\r\n要塞通知設定リセット：デフォルトはONです')
         pass
 
-    #デバッグ 起動確認
-    channel2 = client.get_channel(DEBUG_CHANNEL_ID)
-    if channel2 is None:
+    #デバッグ 起動通知
+    channel_ready = client.get_channel(DEBUG_CHANNEL_ID)
+    if channel_ready is None:
         pass
     else:          
-        await channel2.send('せとうぽくん起動しました')
+        #await channel_ready.send('せとうぽくん起動しました')
+        pass
 
 ##################### 新規参加者の処理 #####################
 @client.event
 async def on_member_join(member):
-         print('誰かが参加しました1')
-         global strtmp
-         print('誰かが参加しました2')
-         channel3 = client.get_channel(DEBUG_CHANNEL_ID)
-         print('誰かが参加しました3')
-         if channel3 is None:
-                  pass
-         else:
-                  print(f'参加したサーバーIDはこちら-->[{member.guild.id}]')
-                  if member.guild.id != 751149121284603935:
-                           print('違うサーバーに入っているよ')
-                           return
+         
+        global strtmp
+        global strpng
+        global strtit
+        global intcol
+
+        # 通知用チャンネル取得
+        channel_join = client.get_channel(DEBUG_CHANNEL_ID)
+        if channel_join is None:
+            pass
+        else:
+            print(f'参加したサーバーID-->[{member.guild.id}]')
+            if member.guild.id != DEBUG_SERVER_ID:
+                print('別サーバーに入っているのでスキップ')
+                return
                   
-                  print('誰かが参加しました4')
-                  #ようこそ文
-#                  strtmp = "以下で自己紹介をお願いします\r\n"\
-#                           " <#771510773549629480> \r\n"\
-#                           "\r\n"\
-#                           ":large_blue_diamond: 基本ルール :large_blue_diamond: \r\n"\
-#                           "以下リンクをご確認ください\r\n"\
-#                           "https://discord.com/channels/578209286639976448/581850951682359296/773011836970205204\r\n"\
-#                           "\r\n"\
-#                           ":large_blue_diamond: せとうぽくんについて :large_blue_diamond: \r\n"\
-#                           "以下コマンドでご確認ください\r\n/せとうぽ\r\n"\
-#                           "\r\n"\
-#                           "不明点は気軽に連絡ください\r\n"
-                  embed3 = discord.Embed(title="ようこそ せとうぽへ",description="",color=0x4169E1)
-                  print('誰かが参加しました5')
-                  embed3.add_field(name=f":sparkles:{member.name}さん:sparkles:\r\nご参加ありがとうございます",value=strtmp,inline=False)
-                  print('誰かが参加しました6')
-                  embed3.set_thumbnail(url="https://img.altema.jp/altema/uploads/2019/03/2019y03m07d_1405336875.png")
-                  print('誰かが参加しました7')
-                  await channel3.send(embed=embed3)
-                  print('誰かが参加しました8')
+            embed_join = discord.Embed(title=strtit,description="",color=intcol)
+            embed_join.add_field(name=f":sparkles:{member.name}さん:sparkles:\r\nご参加ありがとうございます",value=strtmp,inline=False)
+            embed_join.set_thumbnail(url=strpng)
+            await channel_join.send(embed=embed_join)
+        return
          
 ##################### メッセージ受信時の処理 #####################
 @client.event
 async def on_message(message):
     
-    # グローバル変数定義
     global g_yousai_notice_flg
-    global embed
-    global embed2
+    global embed_help
+    global embed_kanbu_help
+    global embed_test
     
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
@@ -157,8 +155,8 @@ async def on_message(message):
     if message.channel.id == KANBU_CHANNEL_ID:
         # 管理部用ヘルプ
         if message.content == '/せとうぽ':
-            await message.channel.send(embed=embed)
-            await message.channel.send(embed=embed2)
+            await message.channel.send(embed=embed_help)
+            await message.channel.send(embed=embed_kanbu_help)
             return
         
         # 要塞通知ON設定
@@ -178,10 +176,15 @@ async def on_message(message):
                 g_yousai_notice_flg = 0
                 await message.channel.send('要塞通知をオフに設定しました')
             return
-    
+        
+        # テストコマンド
+        if message.content == '/せとうぽ テスト':
+            await message.channel.send(embed=embed_test)
+            return
+
     ##################### 全チャンネル #####################
     if message.content == '/せとうぽ':
-        await message.channel.send(embed=embed)
+        await message.channel.send(embed=embed_help)
         return
     
     if message.content == '/せとうぽ ぜくの装備消去':
@@ -206,29 +209,7 @@ async def on_message(message):
         else:
             await message.channel.send('あなたは占えませんでした')
         return
-    
-    if message.content == '/せとうぽ テスト':
-        await message.channel.send(embed=embed_t)
-        return
-    
-#        var1 = config_ini['DEFAULT']['User']
-#        await message.channel.send(var1)
-#        return
-
-#ループ処理実行
+        
+#実行
 loop.start()
 client.run(token)
-
-#bot = commands.Bot(command_prefix='/')
-
-#@bot.event
-#async def on_command_error(ctx, error):
-#    orig_error = getattr(error, "original", error)
-#    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-#    await ctx.send(error_msg)
-
-#@bot.command()
-#async def ping(ctx):
-#    await ctx.send('pong')
-
-#bot.run(token)
