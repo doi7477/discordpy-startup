49fea407313ad7fe662f03b023cbc01b62604fba
doi7477 <70732724+doi7477@users.noreply.github.com>
2021-06-08 10:52:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -563,7 +563,7 @@ async def on_message(message):
     if message.content == '/せとうぽ GA':
         print('GAコマンド実行処理')
         #ga_result = random.choice(ga_list, w=ga_weight, k=11)
-        ga_result = random.choice(ga_list, k=11)
+        ga_result = random.choice(ga_list)
         ga_result_str = ga_date+ '\r\n' + ga_result
         await message.channel.send(ga_result_str)
         return
